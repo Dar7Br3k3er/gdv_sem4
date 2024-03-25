205a76fcb59af8dc88a12e60cb91cf5a3a75dfec
Marvin Veltz <mveltz@web.de>
2024-03-25 17:21:35 +0100

    Completet Task 1 and 2

--- tutorials/src/01-img-manipulation.problem.py
+++ tutorials/src/01-img-manipulation.problem.py
@@ -4,9 +4,9 @@
 # Load, resize and rotate an image. And display it to the screen.
 
 # TODO First step is to import the opencv module which is called 'cv2'
-
+import cv2
 # TODO Check the opencv version
-
+print(cv2.__version__)
 # TODO Load an image with image reading modes using 'imread'
 # cv2.IMREAD_UNCHANGED  - If set, return the loaded image as is (with alpha
 #                         channel, otherwise it gets cropped). Ignore EXIF
@@ -15,11 +15,13 @@
 #                         grayscale image (codec internal conversion).
 # cv2.IMREAD_COLOR      - If set, always convert image to the 3 channel BGR
 #                         color image.
-
+img = cv2.imread("tutorials\data\images\logo.png",cv2.IMREAD_COLOR)
 # TODO Resize image with 'resize'
-
+img = cv2.resize(img, (500,500))
 # TODO Rotate image (but keep it rectangular) with 'rotate'
-
+img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 # TODO Save image with 'imwrite'
-
+cv2.imwrite("file_tutorial_eins.png", img)
 # TODO Show the image with 'imshow'
+cv2.imshow("My Image",img)
+cv2.waitKey(0)
--- tutorials/src/02-pixel-manipulation.problem.py
+++ tutorials/src/02-pixel-manipulation.problem.py
@@ -8,27 +8,42 @@ import cv2
 import numpy as np
 
 # TODO Loading images in grey and color
-
+img_gray = cv2.imread("tutorials\data\images\logo.png", cv2.IMREAD_GRAYSCALE)
+img_color = cv2.imread("tutorials\data\images\logo.png", cv2.IMREAD_COLOR)
 # TODO Do some print out about the loaded data using type, dtype and shape
-
+#Type
+print(type(img_gray))
+print(type(img_color))
+#dtype
+print(img_gray.dtype)
+print(img_color.dtype)
+#shape
+print(img_gray.shape)
+print(img_color.shape)
 # TODO Continue with the grayscale image
-
+img = img_gray
 # TODO Extract the size or resolution of the image
-
+h, w =img.shape
+print("Width =", w)
+print("Height =" ,h)
 # TODO Resize image
-
+img = cv2.resize(img, (8,4))
 # Row and column access, see https://numpy.org/doc/stable/reference/arrays.ndarray.html for general access on ndarrays
 # TODO Print first row
-
+print(img[0,:])
 # TODO Print first column
-
+print(img[:,0])
 # TODO Continue with the color image
-
-# TODO Set an area of the image to black
-
+img = img_color
+print(img.shape)
+# TODO Set an area of the image to black [rows=Zeilen -> colums-Spalten]
+img[50:100,:] = (0,0,0)
 # TODO Show the image and wait until key pressed
+cv2.imshow("My Image", img)
+cv2.waitKey(0)
 
 # TODO Find all used colors in the image
+print()
 
 # TODO Copy one part of an image into another one
 
